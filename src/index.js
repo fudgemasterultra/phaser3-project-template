3a6da45687f12a559b815b51df8e94b7cdfce8b3
fudgemasterultra <79956447+fudgemasterultra@users.noreply.github.com>
2022-04-24 23:31:01 -0400

    --
    --

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import Player from './assets/dude.png'
 class MyGame extends Phaser.Scene
 {
 
-    
+
 
     constructor ()
     {
@@ -20,97 +20,20 @@ class MyGame extends Phaser.Scene
 
      preload ()
     {
-        this.load.image('sky', Sky)
-        this.load.image('platform', Platform)
-        this.load.image('star', Star)
-        this.load.image('bomb', Bomb)
-        this.load.spritesheet('player', Player, {
-            frameWidth: 32,
-            frameHeight: 48
-        })
+        
     }
       
      create ()
     {
-        
-        this.add.image(400,300, 'sky')
-        this.platforms = this.physics.add.staticGroup();
-        let platforms = this.platforms
-        platforms.create(400,568, 'platform').setScale(4).refreshBody();
 
-        platforms.create(600, 400, 'platform');
-        platforms.create(50, 250, 'platform');
-        platforms.create(750, 220, 'platform');
-        this.player = this.physics.add.sprite(100,450, 'player');
-        let player = this.player
-        player.setBounce(.2)
-        //Check if this collides with outer ring of world
-        player.setCollideWorldBounds(true)
-
-        this.physics.add.collider(player, platforms)
-        this.anims.create({
-            key: 'left',
-            frames: this.anims.generateFrameNumbers('player', {
-                start: 0,
-                end: 3
-            }),
-            frameRate: 10,
-            repeat : -1
-        });
-        this.anims.create({
-            key:  'turn',
-            frames: [{
-                key:'player',
-                frame: 4
-            }],
-            frameRate: 20,
-        });
-        this.anims.create({
-            key: 'turn',
-            frames: [{
-                key: 'player',
-                frame: 4,
-            }],
-            frameRate: 20
-        });
-        this.anims.create({
-            key: 'right',
-            frames: this.anims.generateFrameNumbers('player', { start: 5, end: 8 }),
-            frameRate: 10,
-            repeat: -1
-        });
         
+
     }
     
          update(){
-            let cursors = this.input.keyboard.createCursorKeys();
-            if (cursors.left.isDown)
-{
-    this.player.setVelocityX(-160);
-
-    this.player.anims.play('left', true);
-}
-else if (cursors.right.isDown)
-{
-    this.player.setVelocityX(160);
-
-    this.player.anims.play('right', true);
-}
-else
-{
-    this.player.setVelocityX(0);
-
-    this.player.anims.play('turn');
-}
-
-if (cursors.up.isDown && this.player.body.touching.down)
-{
-    this.player.setVelocityY(-330);
-}
-        }
     
 }
-
+}
 const config = {
     type: Phaser.WEBGL,
     parent: 'phaser-example',
