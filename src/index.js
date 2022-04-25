3ae85e464900481983b558460141d2ae43fa465d
fudgemasterultra <79956447+fudgemasterultra@users.noreply.github.com>
2022-04-24 21:37:57 -0400

    --

--- src/index.js
+++ src/index.js
@@ -1,39 +1,129 @@
 import Phaser from 'phaser';
-import logoImg from './assets/logo.png';
+import Sky from './assets/sky.png'
+import Star from './assets/star.png'
+import Platform from './assets/platform.png'
+import Bomb from './assets/bomb.png'
+import Player from './assets/dude.png'
+
+
+
 
 class MyGame extends Phaser.Scene
 {
+
+
+
     constructor ()
     {
         super();
     }
 
-    preload ()
+     preload ()
     {
-        this.load.image('logo', logoImg);
+        this.load.image('sky', Sky)
+        this.load.image('platform', Platform)
+        this.load.image('star', Star)
+        this.load.image('bomb', Bomb)
+        this.load.spritesheet('player', Player, {
+            frameWidth: 32,
+            frameHeight: 48
+        })
     }
       
-    create ()
+     create ()
     {
-        const logo = this.add.image(400, 150, 'logo');
-      
-        this.tweens.add({
-            targets: logo,
-            y: 450,
-            duration: 2000,
-            ease: "Power2",
-            yoyo: true,
-            loop: -1
+        
+        this.add.image(400,300, 'sky')
+        this.platforms = this.physics.add.staticGroup();
+        let platforms = this.platforms
+        platforms.create(400,568, 'platform').setScale(4).refreshBody();
+
+        platforms.create(600, 400, 'platform');
+        platforms.create(50, 250, 'platform');
+        platforms.create(750, 220, 'platform');
+        this.player = this.physics.add.sprite(100,450, 'player');
+        let player = this.player
+        player.setBounce(.2)
+        //Check if this collides with outer ring of world
+        player.setCollideWorldBounds(true)
+
+        this.physics.add.collider(player, platforms)
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('player', {
+                start: 0,
+                end: 3
+            }),
+            frameRate: 10,
+            repeat : -1
         });
+        this.anims.create({
+            key:  'turn',
+            frames: [{
+                key:'player',
+                frame: 4
+            }],
+            frameRate: 20,
+        });
+        this.anims.create({
+            key: 'turn',
+            frames: [{
+                key: 'player',
+                frame: 4,
+            }],
+            frameRate: 20
+        });
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
+        
     }
+    
+         update(){
+            let cursors = this.input.keyboard.createCursorKeys();
+            if (cursors.left.isDown)
+{
+    this.player.setVelocityX(-160);
+
+    this.player.anims.play('left', true);
+}
+else if (cursors.right.isDown)
+{
+    this.player.setVelocityX(160);
+
+    this.player.anims.play('right', true);
+}
+else
+{
+    this.player.setVelocityX(0);
+
+    this.player.anims.play('turn');
+}
+
+if (cursors.up.isDown && this.player.body.touching.down)
+{
+    this.player.setVelocityY(-330);
+}
+        }
+    
 }
 
 const config = {
-    type: Phaser.AUTO,
+    type: Phaser.WEBGL,
     parent: 'phaser-example',
     width: 800,
     height: 600,
-    scene: MyGame
+    scene: MyGame,
+    physics:{
+        default: 'arcade',
+        arcade: {
+            gravity: {y: 300},
+            debug: true,
+        }
+    },
 };
 
 const game = new Phaser.Game(config);
